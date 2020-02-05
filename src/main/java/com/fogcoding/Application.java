ac66a52871b5d72e38b55f32d624062f2ec88ec6
Andrew <credential.helper>
2020-02-05 19:25:07 +0800

    add templates for thymleaf

---
+++ src/main/java/com/fogcoding/Application.java
@@ -0,0 +1,13 @@
+package com.fogcoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("启动 Spring Boot...");
+        SpringApplication.run(Application.class, args);
+    }
+}
