3a5be43f13d5005ab19d04f79f93e1ea57321289
fogcoding <notesome@outlook.com>
2020-03-09 21:10:46 +0800

    增加log4j的基本用法配置

--- ThymleafParent/src/main/java/com/fogcoding/Application.java
+++ ThymleafParent/src/main/java/com/fogcoding/Application.java
@@ -1,5 +1,7 @@
 package com.fogcoding;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +9,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @MapperScan(value = "com.fogcoding.mapper")
 public class Application {
+
+    Logger logger = LogManager.getLogger(LogManager.ROOT_LOGGER_NAME);
 
     public static void main(String[] args) {
         System.out.println("启动 Spring Boot...");
